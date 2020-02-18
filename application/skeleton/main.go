693266f1f5e3df1c16a7073f17d85ec61a9d09a1
James Halsall <james.halsall@inviqa.com>
2020-02-18 16:07:48 +0000

    Add basic prometheus instrumentation (#19)
    Also fix issues with the ws create project flow.

---
+++ application/skeleton/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
+  "log"
+  "net/http"
+)
+
+func main() {
+  fmt.Println("Service running")
+
+  http.Handle("/metrics", promhttp.Handler())
+  http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    w.WriteHeader(200)
+    w.Write([]byte(`{"status": "OK"}`))
+  })
+
+  err := http.ListenAndServe(":80", nil)
+  if err != nil {
+    log.Fatalf("failed to start HTTP server: %s", err)
+  }
+}
