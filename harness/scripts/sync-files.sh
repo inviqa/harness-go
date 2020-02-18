8da9477bbb202283fbddfa4ec11c955cd1cb3d18
James Halsall <james.halsall@inviqa.com>
2020-02-18 16:56:57 +0000

    Debug

--- harness/scripts/sync-files.sh
+++ harness/scripts/sync-files.sh
@@ -5,9 +5,15 @@ sync()
     local skeleton_dir="$1"
     local project_dir="$2"
 
+    echo "ls on $project_dir:"
+    ls "$project_dir"
+
     if [ ! -f "${project_dir}/go.mod" ]; then
         rsync --exclude='*.twig' --exclude='_twig' -a "$skeleton_dir" "$project_dir"
     fi
+
+    echo "ls on $project_dir:"
+    ls "$project_dir"
 }
 
 sync "$1" "$2"
