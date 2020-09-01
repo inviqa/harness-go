8cee889ac3790fa7e8e06fff1ad12874b7ea4c0a
James Halsall <james.halsall@inviqa.com>
2020-09-01 08:28:36 +0100

    Gracefully shut down the HTTP server on SIGINT, SIGTERM (#48)
    Also adds logrus for better logging support out-of-the-box, and fixes an issue with go.mod template.

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ To use this Go harness:
 1. Create an initial commit, ensuring that `workspace.override.yml` is not added to git.
 1. Store the `workspace.override.yml` contents in a suitable location (such as LastPass).
 
+## Supported Go versions
+
+This harness only ever tests one Go version, which is usually the latest. If you like, you can change your Go version to suit your project requirements by setting the `go.version` attribute in your `workspace.yml`. For example:
+
+    attributes:
+      go:
+        version: 1.13
+
+Be sure to change your generated `go.mod` file in your harness too, as that will contain the Go version that is expected to run your application.
+
 ## Required attributes
 
 When creating a new project, you will be asked to provide the following attributes
--- application/skeleton/main.go
+++ application/skeleton/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"log"
-	"net/http"
+  "context"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
+  log "github.com/sirupsen/logrus"
+  "net/http"
+  "os"
+  "os/signal"
+  "syscall"
+  "time"
 )
 
 func main() {
-	fmt.Println("Service running")
+  var srv http.Server
+  httpWait := make(chan struct{})
+  log.SetFormatter(&log.JSONFormatter{})
+  log.Info("service running")
 
-	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte(`{"status": "OK"}`))
-	})
+  http.Handle("/metrics", promhttp.Handler())
+  http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(`{"status": "OK"}`))
+  })
 
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Fatalf("failed to start HTTP server: %s", err)
-	}
+  go func() {
+    stop := make(chan os.Signal, 1)
+    signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+    <-stop
+    log.Info("shutting down HTTP server")
+    ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+    if err := srv.Shutdown(ctx); err != nil {
+      log.Errorf("HTTP server did not shutdown correctly: %s", err)
+    }
+    close(httpWait)
+  }()
+
+  if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+    log.Fatalf("failed to start HTTP server: %s", err)
+  }
+
+  <-httpWait
 }
