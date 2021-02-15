237d0a065cb0b1d1fd63c3d49eee8970bd28883f
James Halsall <james.halsall@inviqa.com>
2021-02-15 14:20:55 +0000

    Apply overlay after ws install (#93)
    Also cleans up the overlay/skeleton directory contents

--- harness/scripts/apply.sh
+++ harness/scripts/apply.sh
@@ -1,7 +1,9 @@
 function apply() {
     if [ ! -f "go.mod" ]; then
-        rsync --exclude='*.twig' --exclude='_twig' -a ".my127ws/application/skeleton/" .
+        rsync --exclude='*.twig' -a ".my127ws/application/skeleton/" .
     fi
+
+    rsync --exclude='*.twig' -a ".my127ws/application/overlay/" .
 }
 
 apply
