bac07198922e4c15cad93787358248c4717b0568
James Halsall <james.halsall@inviqa.com>
2021-11-22 11:58:44 +0000

    Update ws destroy to handle buildx context removal

--- harness/scripts/destroy.sh
+++ harness/scripts/destroy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 run docker-compose down --rmi local --volumes --remove-orphans
+
+if [[ -z "$CONTEXT" ]]; then
+  docker buildx rm "${CONTEXT}"
+fi
+
 passthru ws cleanup built-images
 run rm -f .my127ws/.flag-built
