a0bfee195144ad749fe0bcdec84fe360b5d93313
James Halsall <james.halsall@inviqa.com>
2021-11-22 14:52:34 +0000

    Simplify

--- harness/scripts/destroy.sh
+++ harness/scripts/destroy.sh
@@ -2,9 +2,5 @@
 
 run docker-compose down --rmi local --volumes --remove-orphans
 
-if [[ -z "$CONTEXT" ]]; then
-  docker buildx rm "${CONTEXT}"
-fi
-
 passthru ws cleanup built-images
 run rm -f .my127ws/.flag-built
